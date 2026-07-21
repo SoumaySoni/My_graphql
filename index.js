69bda931a61233cec6b15f5d1a9446403a688c56
Soumay Soni <soumaysoni1687@gmail.com>
2026-07-22 04:12:27 +0530

    lec-02

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
-const schema = require('./schema/schema.js');
+const express = require('express'); // for server
+const {graphqlHTTP} = require('express-graphql'); // for connect server to graphql
+const schema = require('./schema/schema.js'); // to write schema to show the data from db 
 
 const app = express();
+
 app.use("/graphql-me",graphqlHTTP({
     schema,
-    graphiql: true,
+    graphiql: true, // for showing ui on the port to write and run query 
 }))
 
 app.listen(4000,()=>{
--- schema/schema.js
+++ schema/schema.js
@@ -1,8 +1,29 @@
-const {GraphQLSchema , GraphQLObjectType , GraphQLString} = require('graphql')
+const {GraphQLSchema , GraphQLObjectType , GraphQLString, GraphQLInt} = require('graphql')
+
+const userType = new GraphQLObjectType({
+    name : "User",
+    fields : {
+        id : {type : GraphQLString},
+        name : {type : GraphQLString},
+        age : {type : GraphQLInt},
+    }
+})
 
 const RootQuery = new GraphQLObjectType({
     name : "RootQueryType",
     fields : {
+        user:{
+            type : userType,
+            args : {id:{type : GraphQLString}},
+            resolve(parent,args){
+                const users = [
+                    {id:'1', name:"soumay",age:"24"},
+                    {id:'2', name:"mayu",age:"25"}
+                ]
+
+                return users.find(user => user.id === args.id)
+            }
+        },
         hello:{
             type: GraphQLString,
             resolve(){
