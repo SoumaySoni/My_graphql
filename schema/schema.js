f32be3c1e509f484b5ce5ca9fdf5b5dff2db4ab6
Soumay Soni <soumaysoni1687@gmail.com>
2026-07-24 03:57:30 +0530

    lec-03

--- schema/schema.js
+++ schema/schema.js
@@ -9,6 +9,29 @@ const userType = new GraphQLObjectType({
     }
 })
 
+const users = [{id:'1', name:"soumay",age:24},{id:'2', name:"mayu",age:25}]
+
+// mutattion
+const addUser = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        addUser: {
+            type : userType,
+            args: {name:{type: GraphQLString },age :{type: GraphQLInt}},
+            resolve(parent, args){
+                const user = {
+                    id : users.length + 1 +"",
+                    name : args.name,
+                    age : args.age
+                }
+                users.push(user)
+                return user
+            }
+             
+        }
+    }
+})
+
 const RootQuery = new GraphQLObjectType({
     name : "RootQueryType",
     fields : {
@@ -16,11 +39,6 @@ const RootQuery = new GraphQLObjectType({
             type : userType,
             args : {id:{type : GraphQLString}},
             resolve(parent,args){
-                const users = [
-                    {id:'1', name:"soumay",age:"24"},
-                    {id:'2', name:"mayu",age:"25"}
-                ]
-
                 return users.find(user => user.id === args.id)
             }
         },
@@ -34,5 +52,6 @@ const RootQuery = new GraphQLObjectType({
 })
 
 module.exports = new GraphQLSchema({
-    query:RootQuery
+    query:RootQuery,
+    mutation: addUser
 });
